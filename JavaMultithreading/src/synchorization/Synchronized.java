cc4bcb5442a7c3dcca0f5e360e6e2dc5f0c66ce0
Ketan <ketankvishwakkarma@gmail.com>
2016-09-09 13:30:46 +0530

    synchronized keyword implimentation

---
+++ JavaMultithreading/src/synchorization/Synchronized.java
@@ -0,0 +1,46 @@
+package synchorization;
+
+public class Synchronized {
+
+	int count = 0;
+
+	private synchronized void increment() {
+		count++;
+	}
+
+	public static void main(String[] args) {
+		Synchronized s = new Synchronized();
+		s.doWork();
+	}
+
+	public void doWork() {
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 10000; i++)
+					increment();
+			}
+
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 10000; i++)
+					increment();
+			}
+
+		});
+
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Count is "+count);
+	}
+}
